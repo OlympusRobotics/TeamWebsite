fd465455cf9d2e7be36fb7f42de7f45ec4ac5a5a
Abdullah Mohammed <aamohammed4556@gmail.com>
2023-04-10 17:31:09 +0000

    loading bar

--- index.js
+++ index.js
@@ -2,3 +2,9 @@
 import flamethrower from 'flamethrower-router';
 
 const router = flamethrower({ prefetch: 'visible', log: false, pageTransitions: false });
+
+// loading bar
+window.addEventListener('flamethrower:router:fetch-progress', ({ detail }) => {
+	const progressBar = document.getElementById('load');
+	progressBar.style.width = detail.progress + '%';
+});
