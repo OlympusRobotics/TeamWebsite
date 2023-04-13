1d95f520c27e74a57fc67934af7048929e8fcfa4
Abdullah Mohammed <aamohammed4556@gmail.com>
2023-04-13 18:31:16 +0000

    join.html page

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 // required because parcel cannot build inline js tags
-import flamethrower from 'flamethrower-router';
+import flamethrower from "flamethrower-router";
 
-const router = flamethrower({ prefetch: 'visible', log: false, pageTransitions: false });
-
-// loading bar
-window.addEventListener('flamethrower:router:fetch-progress', ({ detail }) => {
-	const progressBar = document.getElementById('load');
-	progressBar.style.width = detail.progress + '%';
+const router = flamethrower({
+  prefetch: "visible",
+  log: false,
+  pageTransitions: false,
 });
 
-(function () {
-    var burger = document.querySelector('.nav-toggle');
-    var menu = document.querySelector('.nav-menu');
-    burger.addEventListener('click', function () {
-        burger.classList.toggle('is-active');
-        menu.classList.toggle('is-active');
-    });
-})();
+// loading bar
+window.addEventListener("flamethrower:router:fetch-progress", ({ detail }) => {
+  const progressBar = document.getElementById("load");
+  progressBar.style.width = detail.progress + "%";
+});
+
+const burgers = function () {
+  var burger = document.querySelector(".nav-toggle");
+  var menu = document.querySelector(".nav-menu");
+  console.log(burger);
+  burger.addEventListener("click", function () {
+    burger.classList.toggle("is-active");
+    menu.classList.toggle("is-active");
+  });
+};
+
+burgers();
+window.addEventListener("flamethrower:router:end", () => {
+  burgers();
+});
