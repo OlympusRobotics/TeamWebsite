fb2db6658a7c917f00ae6ca8bbddee3148853aca
Abdullah Mohammed <aamohammed4556@gmail.com>
2023-04-13 15:00:42 +0000

    youtube page

--- index.js
+++ index.js
@@ -8,3 +8,12 @@ window.addEventListener('flamethrower:router:fetch-progress', ({ detail }) => {
 	const progressBar = document.getElementById('load');
 	progressBar.style.width = detail.progress + '%';
 });
+
+(function () {
+    var burger = document.querySelector('.nav-toggle');
+    var menu = document.querySelector('.nav-menu');
+    burger.addEventListener('click', function () {
+        burger.classList.toggle('is-active');
+        menu.classList.toggle('is-active');
+    });
+})();
